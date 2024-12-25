5e16f5246e2e09f14e7d7e8c7b576a62cac65aed
Velascodeuse <sabrinavelasco33@gmail.com>
2024-12-25 11:31:41 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const handleMobileMenu = () => {
+    console.log('handleMobileMenu exécuté');
+    const navMobileMenu = document.getElementById('navMobileMenu');
+    const mobileMenu = document.getElementById('mobileMenu');
+    const mobileMenuClose = document.getElementById('mobileMenuClose');
+
+    if (!navMobileMenu || !mobileMenu || !mobileMenuClose) {
+        console.error("Un ou plusieurs éléments nécessaires ne sont pas trouvés.");
+        return;
+    }
+
+    const openMenu = () =>{
+        console.log('menu ouvert');
+        mobileMenu.classList.add('mobile-menu--open')
+    };
+    const closeMenu = () => {
+        console.log('menu fermé')
+        mobileMenu.classList.remove('mobile-menu--open')
+    };
+
+    navMobileMenu.addEventListener('click',openMenu);
+    mobileMenuClose.addEventListener('click',closeMenu);
+}
+
+handleMobileMenu();
